d2fc653ab21362656674535752639f9b093309b4
herrmanno <O.herrmann92@gmail.com>
2015-10-08 13:33:42 +0200

    bug fix: typo

--- dist/horcrux.js
+++ dist/horcrux.js
@@ -1,4 +1,4 @@
-define(["require", "exports", "horcrux-core", "horcrux-di", "horcrux-flux", "horcrux-base", "horcrux-router", "horcurx-layout"], function (require, exports) {
+define(["require", "exports", "horcrux-core", "horcrux-di", "horcrux-flux", "horcrux-base", "horcrux-router", "horcrux-layout"], function (require, exports) {
 });
 
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJob3JjcnV4LmpzIiwic291cmNlc0NvbnRlbnQiOltdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
