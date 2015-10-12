05e80e1b5db1bd6c5dccf9822dcb61a27ad6c0f4
herrmanno <O.herrmann92@gmail.com>
2015-10-12 11:10:45 +0200

    added dependency: horcrux-http

--- config.js
+++ config.js
@@ -11,15 +11,17 @@ System.config({
     "horcrux-core": "github:hor-crux/horcrux-core@master",
     "horcrux-di": "github:hor-crux/horcrux-di@master",
     "horcrux-flux": "github:hor-crux/horcrux-flux@master",
+    "horcrux-http": "github:hor-crux/horcrux-http@master",
+    "horcrux-layout": "github:hor-crux/horcrux-layout@master",
     "horcrux-router": "github:hor-crux/horcrux-router@master",
-    "horcrux-layout": "github:hor-crux/horcrux-layout@master",    
     "github:hor-crux/horcrux-base@master": {
-      "horcrux-core": "github:hor-crux/horcrux-core@master"
+      "horcrux-core": "github:hor-crux/horcrux-core@master",
+      "observejs": "github:polymer/observe-js@0.5.6"
     },
     "github:hor-crux/horcrux-core@master": {
       "html": "github:hor-crux/html@master",
       "observejs": "github:polymer/observe-js@0.5.6",
-      "webcomponents": "github:webcomponents/webcomponentsjs@0.7.12"
+      "webcomponents": "github:webcomponents/webcomponentsjs@0.7.14"
     },
     "github:hor-crux/horcrux-di@master": {
       "horcrux-core": "github:hor-crux/horcrux-core@master"
@@ -27,6 +29,9 @@ System.config({
     "github:hor-crux/horcrux-flux@master": {
       "horcrux-core": "github:hor-crux/horcrux-core@master",
       "horcrux-di": "github:hor-crux/horcrux-di@master"
+    },
+    "github:hor-crux/horcrux-http@master": {
+      "horcrux-core": "github:hor-crux/horcrux-core@master"
     },
     "github:hor-crux/horcrux-layout@master": {
       "css": "github:systemjs/plugin-css@0.1.18"
